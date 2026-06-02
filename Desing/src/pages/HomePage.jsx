88fd99a6c700a791259a591d3c7ca5e26f986ad7
Olira1 <oliratesgera1@gmail.com>
2026-06-02 09:00:05 +0300

    Create The Page Shell

---
+++ Desing/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import Footer from '../components/layout/Footer'
+import Header from '../components/layout/Header'
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+
+      {/*
+        Step 5 page shell:
+        Keep main intentionally empty for now.
+        Section components will be added from Step 6 onward.
+      */}
+      <main />
+
+      <Footer />
+    </>
+  )
+}
